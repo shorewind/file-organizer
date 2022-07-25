447fd61bbea1520a21e81b78f1ecef4b2cef98c6
shorewind <99942150+shorewind@users.noreply.github.com>
2022-07-24 23:00:32 -0500

    Add remove and clear subfolders option

--- main.py
+++ main.py
@@ -13,24 +13,23 @@ def options():
     print(f'--------OPTIONS----------\n'
         f'1. Set Source Path\n'
         f'2. Set Destination Path\n'
-        f'3. Add Subfolder\n'
-        f'4. MOVE FILES\n'
+        f'3. Subfolder Options\n'
+        f'4. Move Files\n'
         f'5. Exit\n')
         
 
-run_flag = True
-
 print("***FILE ORGANIZER TOOL***")
+run_flag = True
 
 while run_flag:
     fr = open("user-info.txt", "r")
     lines = fr.readlines()
-    print(len(lines))
     if len(lines) < 3:
         fw = open("user-info.txt", "w")
         fw.write('\n\n\n')
         fw.close()
     fr.seek(0)
+    lines = fr.readlines()
     src = lines[0].strip()
     dest = lines[1].strip()
     subfolders = lines[2].strip()
@@ -38,7 +37,7 @@ while run_flag:
     settings()
     options()
 
-    selection = input("Enter your selection: ")
+    selection = input("Enter your number selection: ")
     
     fw = open("user-info.txt", "w")
 
@@ -49,20 +48,66 @@ while run_flag:
         dest = input("Enter your root destination path: ")
         lines[1] = f'{dest}\n'
     elif selection == '3':
-        sf_list = subfolders.split(', ')
-        sf_list = [x for x in sf_list if x]
-        name = input("Enter a subfolder name: ")
-        if len(sf_list) > 0:
-            subfolders = subfolders + ", " + name
-            lines[2] = subfolders
-        else:
-            subfolders = name
-            lines[2] = name
-        sf_list.append(name)
+
+        sf_flag = True
+
+        while sf_flag:
+            print(f'\n----Subfolder Options----\n'
+                f'1. Add Subfolder\n'
+                f'2. Remove Subfolder\n'
+                f'3. Clear Subfolders\n'
+                f'4. Back\n')
+            sf_selection = input("Enter a subfolder option number: ")
+
+            if sf_selection == '1':
+                sf_list = subfolders.split(', ')
+                sf_list = [x for x in sf_list if x]
+                name = input("Enter a subfolder name: ")
+
+                if len(sf_list) > 0:
+                    subfolders = subfolders + ", " + name.upper()
+                    lines[2] = subfolders
+                else:
+                    subfolders = name.upper()
+                    lines[2] = name.upper()
+                sf_list.append(name.upper())
+                print(f'Added "{name.upper()}" folder to settings.')
+
+            elif sf_selection == '2':
+                sf_list = subfolders.split(', ')
+                sf_list = [x for x in sf_list if x]
+                name = input("Enter a subfolder name: ")
+
+                if name.upper() in sf_list:
+                    sf_list.remove(name.upper())
+
+                    if len(sf_list) >= 1:
+                        subfolders = sf_list[0]
+                        for i in range(1, len(sf_list)):
+                            subfolders += ", " + sf_list[i]
+                    else:
+                        subfolders = ''
+                    lines[2] = subfolders
+                    print(f'Removed "{name.upper()}" folder from settings.')
+                else:
+                    print(f'Failed to remove "{name.upper()}" folder.')
+                    break
+
+            elif sf_selection == '3':
+                subfolders = ''
+                print("Cleared subfolders.")
+            else:
+                break   
+
+            settings()
+            sf_run = input("Would you like to continue managing subfolders? (y/n) ")
+        
+            if sf_run.lower() != 'y':
+                sf_flag = False
+
     elif selection == '4':
         try:
             files = os.listdir(src)
-
             moved_files = ''
 
             for file in files:
